bfcd5587ae793d9cca2def90fb3ee446ac5983cd
Olivier Roux <rouxoliv@gmail.com>
2015-11-04 16:25:01 +0100

    last test

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace testaspnet
             
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello  Olivier!");
+                await context.Response.WriteAsync("Hello  Olivier - Last test!");
             });
         }
     }
